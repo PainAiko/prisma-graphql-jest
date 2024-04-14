2eeda2a66e05a6d34d95406bdaa5012d5418ca8d
ONjaniaina <daonja54@gmail.com>
2024-04-15 01:18:03 +0300

    crud rest api finish , incoming graphql

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,33 @@ import { createExpressServer, useContainer } from 'routing-controllers';
 import { Container } from 'typedi';
 import { SampleController } from './controllers/SampleController';
 import { ContactController } from './controllers/contact.controller';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { ContactResolver } from './graphql/resolvers/contact.resolver';
 useContainer(Container);
 
 const app = createExpressServer({
   controllers:[SampleController,ContactController]
 });
 
-app.listen(3002, () => {
-  console.log('Server is running on port 3002');
+async function main() {
+  const schema = await buildSchema({
+    resolvers: [ContactResolver],
+    validate: false,
+    container: Container,
+  });
+
+  const server = new ApolloServer({ schema });
+  await server.start()
+
+  server.applyMiddleware({ app, path: '/graphql' });
+
+  app.listen(3002, () => {
+    console.log('Server is running on port 3002');
+  });
+}
+
+main().catch((error) => {
+  console.log(error, 'error');
 });
+
---
+++ src/repository/contact.repository.ts
@@ -0,0 +1,4 @@
+export class ContactRepository {
+    constructor() {
+    }
+}
